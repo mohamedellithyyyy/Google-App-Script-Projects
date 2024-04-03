e653a451ed92b32d563b2103bbf53f6500c609e2
mohamed ellithy <ellithy201102@gmail.com>
2024-04-03 03:25:12 +0200

    Add

---
+++ Form Generator/README.md
@@ -0,0 +1,37 @@
+# Google Apps Script Form Generator
+
+This Google Apps Script generates a new Google Form based on data from a specified spreadsheet. It clears the existing questions in the form and replaces them with new ones based on the data in the spreadsheet. The script also shuffles the question order and divides questions into multiple pages.
+
+## Usage
+
+1. **Setup**
+   - Make sure you have a Google Form template with the desired structure.
+   - Create a Google Spreadsheet with the data for the new form. The spreadsheet should have the following columns:
+     - Column A: Leave empty for now.
+     - Column B: Question text.
+     - Column C: Leave empty for now.
+     - Column D: Question type (SHORT_ANSWER, MULTIPLE_CHOICE, CHECKBOX, DROPDOWN).
+     - Column E onwards: Options for multiple choice, checkbox, or dropdown questions.
+
+2. **Run the Script**
+   - Open the Google Apps Script editor.
+   - Copy and paste the provided script into the editor.
+   - Save the script and run the `generateNewForm` function.
+
+3. **Review the Form**
+   - Open the newly generated form and review the questions.
+   - Make any necessary adjustments directly in the form editor if needed.
+
+## Important Notes
+
+- This script will replace all existing questions in the form specified by the `formUrl` variable.
+- The script assumes a specific structure for the spreadsheet data. Ensure that your spreadsheet follows this structure for the script to work correctly.
+
+## Author
+
+- Mohamed Ellithy (https://github.com/mohamedellithyyyy)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
---
+++ Form Generator/code.js
@@ -0,0 +1,140 @@
+function generateNewForm() {
+  var formUrl = "https://docs.google.com/forms/d/1WIcccYkz3OKBYGgS45kPqv_yaJhMRlRfQ8UnJAhMY-8/edit";
+  var form = FormApp.openByUrl(formUrl);
+  var items = form.getItems();
+  for (var i = 0; i < items.length; i++) {
+    form.deleteItem(items[i]);
+  }
+
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getSheetByName('Customer Support Trial');
+  if (!sheet) {
+    Logger.log("Sheet 'Customer Support Trial' not found.");
+    return;
+  }
+
+  var dataRange = sheet.getDataRange();
+  var values = dataRange.getValues();
+  // Shuffle question order
+  form.setShuffleQuestions(true);
+
+  
+  for (var i = 1; i <= 5; i++) { // Loop for the first 5 questions
+    if (values[i][0] != "") {
+      var question = values[i][1];
+      var questionType = values[i][3];
+      var formItem;
+      if (questionType == "SHORT_ANSWER") {
+        formItem = form.addTextItem();
+      } else if (questionType == "MULTIPLE_CHOICE") {
+        formItem = form.addMultipleChoiceItem();
+      } else if (questionType == "CHECKBOX") {
+        formItem = form.addCheckboxItem();
+      } else if (questionType == "DROPDOWN") {
+        formItem = form.addListItem();
+      } else {
+        Logger.log("Unknown question type: " + questionType + question);
+        continue; // Skip this iteration if the question type is unknown
+      }
+      if (!formItem) {
+        Logger.log("Failed to create form item for question: " + question);
+        continue; 
+      }
+      formItem.setTitle(question);
+      var choices = [];
+      for (var j = 5; j < values[i].length; j++) {
+        if (values[i][j] !== "") {
+          var choice = values[i][j];
+          choices.push(choice);
+        }
+      }
+      if (choices.length > 0) {
+        var choiceObjects = [];
+        for (var k = 0; k < choices.length; k++) {
+          choiceObjects.push(formItem.createChoice(choices[k]));
+        }
+        formItem.setChoices(choiceObjects);
+      }
+      formItem.setRequired(true);
+    } else {
+      Logger.log("Empty cell encountered at row " + i);
+      break;
+    }
+  }
+  // Define page titles and question ranges
+  var pages = [
+    { title: 'Questions', startRow: 6, endRow: 15 ,navigationType: FormApp.PageNavigationType.CONTINUE },
+    { title: 'Questions', startRow: 16, endRow: 25 ,navigationType: FormApp.PageNavigationType.SUBMIT },
+    { title: 'Questions', startRow: 26, endRow: 35 ,navigationType: FormApp.PageNavigationType.SUBMIT },
+    { title: 'Questions', startRow: 36, endRow: 45 ,navigationType: FormApp.PageNavigationType.SUBMIT },
+    { title: 'Questions', startRow: 46, endRow: 55 ,navigationType: FormApp.PageNavigationType.SUBMIT },
+    { title: 'Questions', startRow: 56, endRow: 65 ,navigationType: FormApp.PageNavigationType.SUBMIT }
+  ];
+  // Iterate over pages and add questions
+  pages.forEach(function(page) {
+    var pageBreak = form.addPageBreakItem().setTitle(page.title);
+    pageBreak.setGoToPage(page.navigationType);
+    for (var i = page.startRow; i <= page.endRow; i++) {
+      if (values[i][0] != "") {
+        var question = values[i][1];
+        var questionType = values[i][3];
+        var formItem;
+        switch (questionType) {
+          case "SHORT_ANSWER":
+            formItem = form.addTextItem();
+            break;
+          case "MULTIPLE_CHOICE":
+            formItem = form.addMultipleChoiceItem();
+            break;
+          case "CHECKBOX":
+            formItem = form.addCheckboxItem();
+            break;
+          case "DROPDOWN":
+            formItem = form.addListItem();
+            break;
+          default:
+            Logger.log("Unknown question type: " + questionType);
+            continue;
+        }
+        if (!formItem) {
+          Logger.log("Failed to create form item for question: " + question);
+          continue;
+        }
+        formItem.setTitle(question);
+        var choices = [];
+        for (var j = 5; j < values[i].length; j++) {
+          if (values[i][j] !== "") {
+            var choice = values[i][j];
+            choices.push(choice);
+          }
+        }
+        var num_true=values[i][2];
+        if (choices.length > 0) {
+          var choiceObjects = [];
+          for (var k = 0; k < choices.length; k++) {
+            if(k<=num_true)
+            {
+              choiceObjects.push(formItem.createChoice(choices[k],true));
+            }
+            else
+            {
+              choiceObjects.push(formItem.createChoice(choices[k],false));
+            }
+          }
+          // Randomize the answers
+          for (let i = choiceObjects.length - 1; i > 0; i--) 
+          {
+            const j = Math.floor(Math.random() * (i + 1));
+            [choiceObjects[i], choiceObjects[j]] = [choiceObjects[j], choiceObjects[i]];
+          }
+          
+          formItem.setChoices(choiceObjects);
+        }
+        formItem.setPoints(1);
+        formItem.setRequired(true);
+        
+      }
+    }
+  }); // <-- Closing brace for the 'forEach' function
+  Logger.log("Form creation completed.");
+}
